80ef61c49b544c4b448273614dc35b394f73af53
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-04-19 15:49:14 -0400

    Add create new contact feature

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,4 +3,21 @@ class ContactsController < ApplicationController
 		@contacts = Contact.all
 		render 'index'
 	end
+
+	def new
+		render 'new'
+	end
+
+	def create
+	contact = Contact.new(
+      :name => params[:contact][:name],
+      :address => params[:contact][:address],
+      :phone => params[:contact][:phone],
+      :email => params[:contact][:email])
+
+    # Now we save the contact
+    contact.save
+
+    redirect_to("/contacts")
+	end
 end
