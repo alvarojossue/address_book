81f0e0fb00ca0212e3caa34b270b5d42216360a6
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-04-19 15:06:52 -0400

    Initial commit. Add Rails and database structures with seeds

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Contact.create(name: 'Alvaro Castillo', address: '1111 Collins Ave, Miami, FL', phone: '786-555-5555', email: 'alvaro@gmail.com')
+Contact.create(name: 'Sarah Abogabir', address: '1332 Madison Ave, Orlando, FL', phone: '786-555-8822', email: 'sarah@gmail.com')
+Contact.create(name: 'Andreza Feu', address: '3241 Brickell Ave, Miami, FL', phone: '786-555-4322', email: 'andreza@gmail.com')
+Contact.create(name: 'Carlos Cardo', address: '7882 Regan Ave, Doral, FL', phone: '786-555-0987', email: 'carlos@gmail.com')
